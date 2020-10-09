2a1640833a888ed07a6f39a462ada17da569e4fe
antoine <21809219@etu.unicaen.fr>
2020-10-09 22:08:27 +0200

    init unity project

---
+++ space idle/Assets/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    private Text textCredit;
+    public double credit;
+
+    public Text TextCredit { get => textCredit; set => textCredit = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.credit = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.TextCredit.text = "Credits : " + credit;
+    }
+
+    void shoot()
+    {
+        credit++;
+    }
+
+}
